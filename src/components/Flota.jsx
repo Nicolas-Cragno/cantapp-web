db12671bab1705e11858a3c2e47b4d2cb07e205e
Nicolas Cragno <nicolaslautaro.cragno@gmail.com>
2025-10-13 16:56:44 -0300

    asa

--- src/components/Flota.jsx
+++ src/components/Flota.jsx
@@ -1,19 +1,21 @@
 import "./css/GroupCards.css";
 import Card from "./cards/Card";
 import { useEffect, useState } from "react";
-import { listarColeccion } from "../functions/db-functions";
 import { FaSpinner } from "react-icons/fa";
 import ModalVehiculo from "./modales/ModalVehiculo";
+import ModalPersona from "./modales/ModalPersona";
 import { useData } from "../context/DataContext";
 
 const Flota = () => {
-  const { tractores, furgones, vehiculos } = useData();
+  const { tractores, furgones, vehiculos, personas } = useData();
+  const [cantPersonas, setPersonas] = useState(0);
   const [cantTractores, setTractores] = useState(0);
   const [cantFurgones, setFurgones] = useState(0);
   const [cantVehiculos, setVehiculos] = useState(0);
   const [modalTractoresVisible, setModalTractoresVisible] = useState(false);
   const [modalFurgonesVisible, setModalFurgonesVisible] = useState(false);
   const [modalVehiculosVisible, setModalVehiculosVisible] = useState(false);
+  const [modalPersonasVisible, setModalPersonasVisible] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -28,6 +30,9 @@ const Flota = () => {
         );
         setVehiculos(
           vehiculos.filter((vh) => vh.estado === true || vh.estado === 1).length
+        );
+        setPersonas(
+          personas.filter((p) => p.estado === true || p.estado === 1).length
         );
       } catch (error) {
         console.error("Error al obtener datos con caché: ", error);
@@ -51,6 +56,10 @@ const Flota = () => {
     setModalVehiculosVisible(false);
   };
 
+  const cerrarModalPersonas = () => {
+    setModalPersonasVisible(false);
+  };
+
   return (
     <div className="container group-card">
       <div className="row">
@@ -58,6 +67,14 @@ const Flota = () => {
           <h1>FLOTA</h1>
         </div>
         <div className="col-auto gcard">
+          <Card
+            title="Empleados"
+            value={
+              loading ? <FaSpinner className="spinner" /> : `${cantPersonas}`
+            }
+            // route="/personas"
+            onClick={() => setModalPersonasVisible(true)}
+          />
           <Card
             title="Tractores"
             value={
@@ -108,6 +125,10 @@ const Flota = () => {
                 propios={true}
                 onClose={cerrarModalVehiculos}
               />
+            )}
+
+            {modalPersonasVisible && (
+              <ModalPersona onClose={cerrarModalPersonas} />
             )}
           </div>
         </div>
