69ed054651af3a047f6179b9e5c92c86c141e337
Nicolas Cragno <nicolaslautaro.cragno@gmail.com>
2025-11-25 13:44:23 -0300

    inicio de satelital

--- src/components/forms/FormEventoSatelital.jsx
+++ src/components/forms/FormEventoSatelital.jsx
@@ -15,6 +15,7 @@ import InputValidator from "../devs/InputValidator";
 import "./css/Forms.css";
 
 const FormEventoSatelital = ({ evento = {}, onClose, onGuardar }) => {
+  //const evento = elemento
   const area = "satelital";
   const { personas, tractores, furgones } = useData();
   const [modalTractorVisible, setModalTractorVisible] = useState(false);
@@ -26,8 +27,14 @@ const FormEventoSatelital = ({ evento = {}, onClose, onGuardar }) => {
     fecha: formatearFecha(evento.fecha),
     hora: formatearHora(evento.fecha),
     persona: evento.persona ? String(evento.persona) : "",
+    personaRelacionada: evento.personaRelacionada
+      ? String(evento.personaRelacionada)
+      : "",
     tractor: evento.tractor || "",
+    tractorRelacionado: evento.tractorRelacionado || "",
     furgon: evento.furgon || "",
+    cliente: evento.cliente || "",
+    ubicacion: evento.ubicacion || "",
     detalle: evento.detalle || "",
     area: area,
   });
@@ -75,29 +82,30 @@ const FormEventoSatelital = ({ evento = {}, onClose, onGuardar }) => {
 
     try {
       let fechaParaGuardar;
-
-      if (evento.id) {
-        if (evento.fecha.toDate) {
-          fechaParaGuardar = evento.fecha.toDate();
-        } else {
-          fechaParaGuardar = new Date(evento.fecha);
-        }
+      if (evento?.id && evento?.fecha) {
+        fechaParaGuardar = evento.fecha.toDate
+          ? evento.fecha.toDate()
+          : new Date(evento.fecha);
       } else {
         fechaParaGuardar = new Date();
-      }
-
-      if (isNaN(fechaParaGuardar.getTime())) {
-        throw new Error("La fecha es inválida");
       }
 
       const datosAGuardar = {
         ...formData,
         fecha: evento.id ? evento.fecha : fechaParaGuardar,
         persona: formData.persona ? Number(formData.persona) : null,
+        personaRelacionada: formData.personaRelacionada
+          ? Number(formData.personaRelacionada)
+          : null,
         tractor: formData.tractor ? Number(formData.tractor) : null,
+        tractorRelacionado: formData.tractorRelacionado
+          ? Number(formData.tractorRelacionado)
+          : null,
         furgon: formData.furgon ? Number(formData.furgon) : null,
+        cliente: formData.cliente ? formData.cliente.toUpperCase() : null,
         area: area,
-        detalle: formData.detalle ? formData.detalle : null,
+        ubicacion: formData.ubicacion ? formData.ubicacion.toUpperCase() : null,
+        detalle: formData.detalle ? formData.detalle.toUpperCase() : null,
       };
 
       await agregarEvento(datosAGuardar, area, evento.id);
@@ -136,157 +144,300 @@ const FormEventoSatelital = ({ evento = {}, onClose, onGuardar }) => {
           <span>{evento.id ? formData.hora + " HS" : ""}</span>
         </div>
         <form onSubmit={handleSubmit} className="form-submit">
-          <label>
-            Tipo * <InputValidator campo={formData.tipo} />
-            <Select
-              options={subtiposDisponibles.map((sub) =>
-                typeof sub === "string"
-                  ? { value: sub, label: sub }
-                  : { value: sub.tipo, label: sub.tipo }
-              )}
-              value={
-                formData.tipo
-                  ? { value: formData.tipo, label: formData.tipo }
-                  : null
-              }
-              onChange={(opt) =>
-                handleChange({
-                  target: { name: "tipo", value: opt ? opt.value : "" },
-                })
-              }
-              placeholder=""
-              isClearable
-              required
-            />
-          </label>
-          <label>
-            Persona / Empleado <InputValidator campo={formData.persona} />
-            <div className="select-with-button">
+          <p className="ficha-info-title">
+            <strong>Información principal</strong>
+          </p>
+          <div className="ficha-info">
+            {/* Tipo */}
+            <label>
+              Tipo * <InputValidator campo={formData.tipo} />
               <Select
-                className="select-grow"
-                options={personas
-                  .map((p) => ({
-                    value: p.id,
-                    label: `${p.apellido} ${p.nombres} (DNI: ${p.dni})`,
-                    apellido: p.apellido, //para odenar
-                  }))
-                  .sort((a, b) => a.apellido.localeCompare(b.apellido))}
+                options={subtiposDisponibles.map((sub) =>
+                  typeof sub === "string"
+                    ? { value: sub, label: sub }
+                    : { value: sub.tipo, label: sub.tipo }
+                )}
                 value={
-                  formData.persona
-                    ? {
-                        value: formData.persona,
-                        label:
-                          personas.find((p) => p.id === formData.persona)
-                            ?.apellido +
-                          " " +
-                          personas.find((p) => p.id === formData.persona)
-                            ?.nombres +
-                          ` (DNI: ${formData.persona})`,
-                      }
+                  formData.tipo
+                    ? { value: formData.tipo, label: formData.tipo }
                     : null
                 }
                 onChange={(opt) =>
-                  setFormData({ ...formData, persona: opt ? opt.value : "" })
+                  handleChange({
+                    target: { name: "tipo", value: opt ? opt.value : "" },
+                  })
                 }
                 placeholder=""
                 isClearable
                 required
               />
-              <TextButton
-                text="+"
-                className="mini-btn"
-                onClick={handleClickPersona}
-              />
-            </div>
-          </label>
-
-          <label>
-            Tractor * <InputValidator campo={formData.tractor} />
-            <div className="select-with-button">
-              <Select
-                className="select-grow"
-                options={tractores
-                  .map((t) => ({
-                    value: t.interno,
-                    label: `${t.dominio} (${t.interno})`,
-                    int: t.interno,
-                  }))
-                  .sort((a, b) => a.int - b.int)}
-                value={
-                  formData.tractor
-                    ? {
-                        value: formData.tractor,
-                        label:
-                          tractores.find((t) => t.interno === formData.tractor)
-                            ?.dominio + ` (${formData.tractor})`,
-                      }
-                    : null
-                }
-                onChange={(opt) =>
+            </label>
+            {/* Ubicación */}
+            <label>
+              Ubicación <InputValidator campo={formData.ubicacion} />
+              <input
+                className="input-grow"
+                type="text"
+                value={formData.ubicacion}
+                onChange={(e) =>
                   setFormData({
                     ...formData,
-                    tractor: opt ? opt.value : "",
+                    ubicacion: e ? String(e.target.value) : "",
                   })
                 }
-                placeholder=""
-                isClearable
               />
-              <TextButton
-                text="+"
-                className="mini-btn"
-                onClick={handleClickTractor}
-              />
-            </div>
-          </label>
-
-          <label>
-            Carga / Furgón <InputValidator campo={formData.furgon} />
-            <div className="select-with-button">
-              <Select
-                className="select-grow"
-                options={furgones
-                  .map((f) => ({
-                    value: f.id,
-                    label: `${f.dominio} (${f.interno ? f.interno : f.marca})`,
-                  }))
-                  .sort((a, b) => a.label.localeCompare(b.label))}
-                value={
-                  formData.furgon
-                    ? (() => {
-                        const seleccion = furgones.find(
-                          (v) => v.id === formData.furgon
-                        );
-                        return seleccion
+            </label>
+            {/* Persona / Empleado */}
+            <label>
+              Persona / Empleado <InputValidator campo={formData.persona} />
+              <div className="select-with-button">
+                <Select
+                  className="select-grow"
+                  options={personas
+                    .map((p) => ({
+                      value: p.id,
+                      label: `${p.apellido} ${p.nombres} (DNI: ${p.dni})`,
+                      apellido: p.apellido, //para odenar
+                    }))
+                    .sort((a, b) => a.apellido.localeCompare(b.apellido))}
+                  value={
+                    formData.persona
+                      ? {
+                          value: formData.persona,
+                          label:
+                            personas.find((p) => p.id === formData.persona)
+                              ?.apellido +
+                            " " +
+                            personas.find((p) => p.id === formData.persona)
+                              ?.nombres +
+                            ` (DNI: ${formData.persona})`,
+                        }
+                      : null
+                  }
+                  onChange={(opt) =>
+                    setFormData({ ...formData, persona: opt ? opt.value : "" })
+                  }
+                  placeholder=""
+                  isClearable
+                  required
+                />
+                <TextButton
+                  text="+"
+                  className="mini-btn"
+                  onClick={handleClickPersona}
+                />
+              </div>
+            </label>
+            {/* Tractor */}
+            <label>
+              Tractor * <InputValidator campo={formData.tractor} />
+              <div className="select-with-button">
+                <Select
+                  className="select-grow"
+                  options={tractores
+                    .map((t) => ({
+                      value: t.interno,
+                      label: `${t.dominio} (${t.interno})`,
+                      int: t.interno,
+                    }))
+                    .sort((a, b) => a.int - b.int)}
+                  value={
+                    formData.tractor
+                      ? {
+                          value: formData.tractor,
+                          label:
+                            tractores.find(
+                              (t) => t.interno === formData.tractor
+                            )?.dominio + ` (${formData.tractor})`,
+                        }
+                      : null
+                  }
+                  onChange={(opt) =>
+                    setFormData({
+                      ...formData,
+                      tractor: opt ? opt.value : "",
+                    })
+                  }
+                  placeholder=""
+                  isClearable
+                />
+                <TextButton
+                  text="+"
+                  className="mini-btn"
+                  onClick={handleClickTractor}
+                />
+              </div>
+            </label>
+          </div>
+          {formData.tipo === "CAMBIO" && (
+            <>
+              <p className="ficha-info-title">
+                <strong>Información del cambio</strong>
+              </p>
+              <div className="ficha-info">
+                {/* 2° Persona / Empleado */}
+                <label>
+                  2° Persona / Empleado{" "}
+                  <InputValidator campo={formData.personaRelacionada} />
+                  <div className="select-with-button">
+                    <Select
+                      className="select-grow"
+                      options={personas
+                        .map((p) => ({
+                          value: p.id,
+                          label: `${p.apellido} ${p.nombres} (DNI: ${p.dni})`,
+                          apellido: p.apellido, //para odenar
+                        }))
+                        .sort((a, b) => a.apellido.localeCompare(b.apellido))}
+                      value={
+                        formData.personaRelacionada
                           ? {
-                              value: seleccion.id,
-                              label: `${seleccion.dominio} (${
-                                seleccion.interno
-                                  ? seleccion.interno
-                                  : seleccion.marca
-                              })`,
+                              value: formData.personaRelacionada,
+                              label:
+                                personas.find(
+                                  (p) => p.id === formData.personaRelacionada
+                                )?.apellido +
+                                " " +
+                                personas.find(
+                                  (p) => p.id === formData.personaRelacionada
+                                )?.nombres +
+                                ` (DNI: ${formData.personaRelacionada})`,
                             }
-                          : null;
-                      })() // 👈 ejecutamos la función acá
-                    : null
-                }
-                onChange={(opt) =>
+                          : null
+                      }
+                      onChange={(opt) =>
+                        setFormData({
+                          ...formData,
+                          personaRelacionada: opt ? opt.value : "",
+                        })
+                      }
+                      placeholder=""
+                      isClearable
+                    />
+                    <TextButton
+                      text="+"
+                      className="mini-btn"
+                      onClick={handleClickPersona}
+                    />
+                  </div>
+                </label>
+                {/* 2° Tractor */}
+                <label>
+                  2° Tractor *{" "}
+                  <InputValidator campo={formData.tractorRelacionado} />
+                  <div className="select-with-button">
+                    <Select
+                      className="select-grow"
+                      options={tractores
+                        .map((t) => ({
+                          value: t.interno,
+                          label: `${t.dominio} (${t.interno})`,
+                          int: t.interno,
+                        }))
+                        .sort((a, b) => a.int - b.int)}
+                      value={
+                        formData.tractorRelacionado
+                          ? {
+                              value: formData.tractorRelacionado,
+                              label:
+                                tractores.find(
+                                  (t) =>
+                                    t.interno === formData.tractorRelacionado
+                                )?.dominio +
+                                ` (${formData.tractorRelacionado})`,
+                            }
+                          : null
+                      }
+                      onChange={(opt) =>
+                        setFormData({
+                          ...formData,
+                          tractorRelacionado: opt ? opt.value : "",
+                        })
+                      }
+                      placeholder=""
+                      isClearable
+                    />
+                    <TextButton
+                      text="+"
+                      className="mini-btn"
+                      onClick={handleClickTractor}
+                    />
+                  </div>
+                </label>
+              </div>
+            </>
+          )}
+          <p className="ficha-info-title">
+            <strong>Información de carga</strong>
+          </p>
+          <div className="ficha-info">
+            {/* Carga / Furgón */}
+            <label>
+              Carga / Furgón <InputValidator campo={formData.furgon} />
+              <div className="select-with-button">
+                <Select
+                  className="select-grow"
+                  options={furgones
+                    .map((f) => ({
+                      value: f.id,
+                      label: `${f.dominio} (${
+                        f.interno ? f.interno : f.marca
+                      })`,
+                    }))
+                    .sort((a, b) => a.label.localeCompare(b.label))}
+                  value={
+                    formData.furgon
+                      ? (() => {
+                          const seleccion = furgones.find(
+                            (v) => v.id === String(formData.furgon)
+                          );
+                          return seleccion
+                            ? {
+                                value: seleccion.id,
+                                label: `${seleccion.dominio} (${
+                                  seleccion.interno
+                                    ? seleccion.interno
+                                    : seleccion.marca
+                                })`,
+                              }
+                            : null;
+                        })() // 👈 ejecutamos la función acá
+                      : null
+                  }
+                  onChange={(opt) =>
+                    setFormData({
+                      ...formData,
+                      furgon: opt ? opt.value : "",
+                    })
+                  }
+                  placeholder="Seleccionar vehículo..."
+                  isClearable
+                />
+
+                <TextButton
+                  text="+"
+                  className="mini-btn"
+                  onClick={handleClickFurgon}
+                />
+              </div>
+            </label>
+            {/* Cliente */}
+            <label>
+              Cliente <InputValidator campo={formData.cliente} />
+              <input
+                className="input-grow"
+                type="text"
+                value={formData.cliente}
+                onChange={(e) =>
                   setFormData({
                     ...formData,
-                    furgon: opt ? opt.value : "",
+                    cliente: e ? String(e.target.value) : "",
                   })
                 }
-                placeholder="Seleccionar vehículo..."
-                isClearable
+                disabled={!formData.furgon}
               />
-
-              <TextButton
-                text="+"
-                className="mini-btn"
-                onClick={handleClickFurgon}
-              />
-            </div>
-          </label>
-
+            </label>
+          </div>
           <label>
             Detalle <InputValidator campo={formData.detalle} />
             <textarea
@@ -308,12 +459,13 @@ const FormEventoSatelital = ({ evento = {}, onClose, onGuardar }) => {
           </div>
 
           <div className="form-buttons">
-            <button type="submit" disabled={uploading}>
-              {uploading ? "Guardando..." : "Guardar"}
-            </button>
-            <button type="button" onClick={onClose}>
-              Cancelar
-            </button>
+            <TextButton
+              text={uploading ? "Guardando..." : "Guardar"}
+              onClick={handleSubmit}
+              type="submit"
+              disabled={uploading}
+            />
+            <TextButton text="Cancelar" onClick={onClose} type="button" />
           </div>
         </form>
       </div>
